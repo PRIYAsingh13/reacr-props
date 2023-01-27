2d0e796f0e5c22912f3937d581a44ee167884edd
PRIYAsingh13 <ps597934@gmail.com>
2023-01-27 15:40:01 +0530

    components

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List(){
+    return(
+        <ol>
+            <li>one</li>
+            <li>one</li>
+            <li>one</li>
+            <li>one</li>
+            <li>one</li>
+
+        </ol>
+    )
+}
+export default List;
